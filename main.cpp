af13aa16ecc1f12fdf55b3f12f9800f8640e9996
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2025-02-15 12:41:38 +0200

    Adding the base for the INSERT functionality

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@
 #define TABLE_ROW_SEPARATOR '-'
 #define TABLE_COL_SEPARATOR '|'
 #define END_OF_COL_SEPARATOR " |"
+#define COMMA_SPACE_SEPARATOR std::string(", ")
 
 
-std::string repeat(const char&, const std::string::size_type&);
+std::string repeat(const char &, const std::string::size_type &);
 
-std::string addRightPadding(const std::string&, const std::string::size_type&);
+std::string addRightPadding(const std::string &, const std::string::size_type &);
 
-std::string createBetweenRowsRow(const std::string::size_type*, const int&);
+std::string createBetweenRowsRow(const std::string::size_type *, const int &);
 
 
 int main() {
@@ -54,6 +55,8 @@ int main() {
         return 1;
     }
 
+    // TODO: move to a method, add to a class with SQL methods (header file with .cpp impls)
+// #if 0
     PGresult *queryResult = nullptr;
     queryResult = PQexec(connection, "SELECT * FROM pc;");
 
@@ -118,6 +121,66 @@ int main() {
     }
 
     PQclear(queryResult);
+// #endif
+
+#if 0
+    std::string brand, model, processor, storageType, gpu, purchaseDate;
+    int ramSize, storageSize;
+    double price;
+
+    std::cout << "Enter brand: ";
+    std::cin >> brand;
+
+    std::cout << "Enter model: ";
+    std::cin >> model;
+
+    std::cout << "Enter processor: ";
+    std::cin >> processor;
+
+    std::cout << "Enter RAM size: ";
+    std::cin >> ramSize;
+
+    std::cout << "Enter Storage size: ";
+    std::cin >> storageSize;
+
+    std::cout << "Enter Storage type: ";
+    std::cin >> storageType;
+
+    std::cout << "Enter GPU: ";
+    std::cin >> gpu;
+
+    std::cout << "Enter price: ";
+    std::cin >> price;
+
+    std::cout << "Enter purchase date: ";
+    std::cin >> purchaseDate;
+
+    // Parameterized query
+    const char *paramValues[] = {
+        brand.c_str(), model.c_str(),
+        processor.c_str(), std::to_string(ramSize).c_str(),
+        std::to_string(storageSize).c_str(), storageType.c_str(), gpu.c_str(),
+        std::to_string(price).c_str(), purchaseDate.c_str()
+    };
+
+    PGresult *res = PQexecParams(connection,
+                                 "INSERT INTO pc (brand, model, processor, ram_size, storage_size, storage_type, gpu, price, purchase_date) "
+                                 "VALUES ($1, $2, $3, $4::int, $5::int, $6, $7, $8::float, $9::date);",
+                                 9, // Number of parameters
+                                 NULL, // Let PostgreSQL infer parameter types
+                                 paramValues,
+                                 NULL, NULL, 0 // No binary format
+    );
+
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        std::cerr << "INSERT failed: " << PQresultErrorMessage(res) << std::endl;
+    } else {
+        std::cout << "Record inserted successfully!" << std::endl;
+    }
+
+    PQclear(res);
+    PQfinish(connection);
+#endif
 
     return 0;
 }
@@ -147,7 +210,7 @@ std::string addRightPadding(const std::string &valueStr, const std::string::size
 }
 
 
-std::string createBetweenRowsRow(const std::string::size_type* columnWidths, const int& colWidthsSize) {
+std::string createBetweenRowsRow(const std::string::size_type *columnWidths, const int &colWidthsSize) {
     std::stringstream resultStream{};
     resultStream << TABLE_COL_SEPARATOR;
 
