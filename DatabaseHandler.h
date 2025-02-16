eec45b815936bca594d2a1b0fb27969005020f1d
Peter Gerdzhikov <makotsevo.fan@gmail.com>
2025-02-17 00:16:25 +0200

    Moving the SQL function to DatabaseHandler class

---
+++ DatabaseHandler.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <libpq-fe.h>
+#include <string>
+
+
+class DatabaseHandler {
+    PGconn *connection;
+public:
+    explicit DatabaseHandler(PGconn * connection);
+
+    int SELECT_ALL_SQL_QUERY(const std::string &, const std::string &) const;
+
+    int INSERT_SQL_QUERY(const std::string &) const;
+
+    int UPDATE_SQL_QUERY(const std::string &) const;
+
+    int DELETE_SQL_QUERY(const std::string &) const;
+
+    int TRUNCATE_SQL_QUERY(const std::string &) const;
+
+    int DROP_TABLE_SQL_QUERY(const std::string &) const;
+
+    int DROP_DATABASE_SQL_QUERY(const std::string &) const;
+
+    int CREATE_TABLE_SQL_QUERY(const std::string &) const;
+};
